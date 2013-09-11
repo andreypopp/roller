2e94b82a70f9426fcb8a8063333a50c2e62b885f
Andrey Popp <8mayday@gmail.com>
2013-09-11 17:40:38 +0400

    add comments

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 /* global require, Buffer, process, module */
 "use strict";
 
-var fs              = require('fs'),
-    path            = require('path'),
-    rng             = require('crypto').rng,
-    _               = require('underscore'),
-    q               = require('kew'),
-    through         = require('through'),
-    nodeResolve     = require('resolve'),
-    browserResolve  = require('browser-resolve'),
-    detective       = require('detective'),
-    aggregate       = require('stream-aggregate-promise'),
-    asStream        = require('as-stream')
+var fs                = require('fs'),
+    path              = require('path'),
+    rng               = require('crypto').rng,
+    _                 = require('underscore'),
+    q                 = require('kew'),
+    through           = require('through'),
+    nodeResolve       = require('resolve'),
+    browserResolve    = require('browser-resolve'),
+    detective         = require('detective'),
+    aggregate         = require('stream-aggregate-promise'),
+    asStream          = require('as-stream'),
+    transformResolve  = resolveWith.bind(null, nodeResolve)
 
+/**
+ * Resolve module by id
+ *
+ * @param {Function<String, Object, Callback>} resolve
+ * @param {String} id
+ * @param {Object} parent
+ * @return {Promise<Object>} an object with id, filename and package keys
+ */
 function resolveWith(resolve, id, parent) {
   var p = q.defer()
   resolve(id, parent, function(err, filename, pkg) {
@@ -22,16 +31,97 @@ function resolveWith(resolve, id, parent) {
   return p
 }
 
+/**
+ * Run stream transform over module
+ *
+ * Such transforms are compatible with transforms for browserify
+ *
+ * @param {Function<String>} transform a transform factory
+ * @param {Object} mod a module to run transform over
+ * @return {Promise<Object>} a transformed module
+ */
+function runStreamTransform(transform, mod) {
+  return aggregate(asStream(mod.source).pipe(transform(mod.filename)))
+    .then(function(source) {
+      mod.source = source
+      return mod
+    })
+}
+
+/**
+ * Run transform over module
+ *
+ * @param {Function<String>} transform a transform factory
+ * @param {Object} mod a module to run transform over
+ * @return {Promise<Object>} a transformed module
+ */
+function runTransform(transform, mod) {
+  return q.resolve(transform(mod.filename, mod)).then(function(transformed) {
+    if (transformed.source) mod.source = transformed.source
+    if (transformed.deps) mod.deps = _.extend(mod.deps, transformed.deps)
+    return mod
+  })
+}
+
+/**
+ * Load transform from a package
+ *
+ * @param {Object} pkg a package to load transform from
+ * @param {Array<String>} key an array of strings represents a path in a package
+ * @return {Array<String>} a list of transform ids
+ */
+function getTransform(pkg, key) {
+  var n = pkg
+  key.forEach(function (k) { if (n && typeof n === 'object') n = n[k] })
+  return [].concat(n).filter(Boolean)
+}
+
+/**
+ * Load transform relative to specified module
+ *
+ * It tries to resolve transform relative to module's filename first and then
+ * fallback's to process.cwd()
+ *
+ * @param {Object} mod a module
+ * @param {String} transform a transform module id
+ * @return {Promise<Function>} a loaded transform function
+ */
+function loadTransform(mod, transform) {
+  if (!_.isString(transform)) return q.resolve(transform)
+
+  return transformResolve(transform, {basedir: path.dirname(mod.filename)})
+    .fail(function() {
+      return transformResolve(transform, {basedir: process.cwd()})
+    })
+    .then(function(res) {
+      if (!res)
+        throw new Error([
+          'cannot find transform module ', transform,
+          ' while transforming ', mod.filename
+        ].join(''))
+      return require(res.filename)
+    })
+}
+
+/**
+ * Resolve a graph of dependencies for a specified set of modules
+ *
+ * This is compatible with module-deps implementation with a few addition
+ * features
+ *
+ * @param {Array<String|Stream>} mains a list of module ids, paths or streams
+ * @param {Object} opts an options object compatible with module-deps
+ * @return {Stream<Object>} a stream of resolved modules with dependencies
+ */
 module.exports = function(mains, opts) {
   opts = opts || {}
 
   var output = through(),
       basedir = opts.basedir || process.cwd(),
       resolve = resolveWith.bind(null, opts.resolve || browserResolve),
-      transformResolve = resolveWith.bind(null, nodeResolve),
       cache = {},
       seen = {},
-      entries = [].concat(mains).filter(Boolean).map(makeModule)
+      entries = [].concat(mains).filter(Boolean).map(makeMainModule)
 
   q.all(entries.map(function(mod) {return walk(mod, {filename: '/', id: '/'})}))
     .fail(output.emit.bind(output, 'error'))
@@ -39,7 +129,7 @@ module.exports = function(mains, opts) {
 
   return output
 
-  function makeModule(m) {
+  function makeMainModule(m) {
     var filename,
         mod = {entry: true, deps: {}}
     if (typeof m.pipe === 'function') {
@@ -104,6 +194,12 @@ module.exports = function(mains, opts) {
     })
   }
 
+  function walkDeps(mod) {
+    return q.all(Object.keys(mod.deps)
+      .filter(function(depId) { return mod.deps[depId] })
+      .map(function(depId) { return walk({id: depId, deps: {}}, mod) }))
+  }
+
   function extractDeps(filename, mod) {
     if (opts.noParse && opts.noParse.indexOf(filename) > -1) return {}
     var deps = detective(mod.source)
@@ -114,56 +210,6 @@ module.exports = function(mains, opts) {
       deps.forEach(function(dep) { mod.deps[dep.id] = dep.filename })
       return mod
     })
-  }
-
-  function walkDeps(mod) {
-    return q.all(Object.keys(mod.deps)
-      .filter(function(depId) { return mod.deps[depId] })
-      .map(function(depId) { return walk({id: depId, deps: {}}, mod) }))
-  }
-
-  function getTransform(pkg) {
-    if (opts.transformKey) {
-      var n = pkg
-      opts.transformKey.forEach(function (key) {
-        if (n && typeof n === 'object') n = n[key]
-      })
-      return [].concat(n).filter(Boolean)
-    }
-  }
-
-  function loadTransform(mod, transform) {
-    if (!_.isString(transform)) return q.resolve(transform)
-
-    return transformResolve(transform, {basedir: path.dirname(mod.filename)})
-      .fail(function() {
-        return transformResolve(transform, {basedir: process.cwd()})
-      })
-      .then(function(res) {
-        if (!res)
-          throw new Error([
-            'cannot find transform module ', transform,
-            ' while transforming ', mod.filename
-          ].join(''))
-        return require(res.filename)
-      })
-  }
-
-  function doTransform(transform, mod) {
-    if (transform.length === 1) {
-      return aggregate(asStream(mod.source).pipe(transform(mod.filename)))
-        .then(function(source) {
-          mod.source = source
-          return mod
-        })
-    } else {
-      return q.resolve(transform(mod.filename, mod))
-        .then(function(transformed) {
-          if (transformed.source) mod.source = transformed.source
-          if (transformed.deps) mod.deps = _.extend(mod.deps, transformed.deps)
-          return mod
-        })
-    }
   }
 
   function applyTransforms(mod) {
@@ -177,12 +223,19 @@ module.exports = function(mains, opts) {
           return mod
         })
 
-    if (isTopLevel) txs = txs.concat(opts.transform)
-    if (mod.package) txs = txs.concat(getTransform(mod.package))
+    if (isTopLevel)
+      txs = txs.concat(opts.transform)
+
+    if (mod.package && opts.transformKey)
+      txs = txs.concat(getTransform(mod.package, opts.transformKey))
 
     return q.all(txs.filter(Boolean).map(loadTransform.bind(null, mod)))
       .then(function(txs) {
-        txs.forEach(function(t) { p = p.then(doTransform.bind(null, t)) })
+        txs.forEach(function(t) {
+          p = p.then(function(p) {
+            return (t.length === 1 ? runStreamTransform : runTransform)(t, p)
+          })
+        })
         return p
       })
   }
