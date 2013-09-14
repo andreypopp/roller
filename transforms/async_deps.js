d0337b9bc98329c17331cd1e6be5691235ac64f8
Andrey Popp <8mayday@gmail.com>
2013-09-14 17:11:29 +0400

    transforms/async_deps: use require.async calls for dep extraction

--- transforms/async_deps.js
+++ transforms/async_deps.js
@@ -1,10 +1,74 @@
 "use strict";
 
-var detective = require('detective')
+var esprima = require('esprima'),
+    escodegen = require('escodegen')
+
+var traverse = function (node, cb) {
+    if (Array.isArray(node)) {
+        node.forEach(function (x) {
+            if(x != null) {
+                x.parent = node;
+                traverse(x, cb);
+            }
+        });
+    }
+    else if (node && typeof node === 'object') {
+        cb(node);
+
+        Object.keys(node).forEach(function (key) {
+            if (key === 'parent' || !node[key]) return;
+            node[key].parent = node;
+            traverse(node[key], cb);
+        });
+    }
+};
+
+var walk = function (src, cb) {
+    var ast = esprima.parse(src);
+    traverse(ast, cb);
+};
+
+var extract = function (src, opts) {
+    return find(src, opts).strings;
+};
+
+var find = function (src, opts) {
+    if (!opts) opts = {};
+    var word = opts.word === undefined ? 'require' : opts.word;
+    if (typeof src !== 'string') src = String(src);
+    src = '(function(){' + src.replace(/^#![^\n]*\n/, '') + '\n})()';
+
+    function isRequire (node) {
+        var c = node.callee;
+        return c
+            && node.type === 'CallExpression'
+            && escodegen.generate(c) === word
+        ;
+    }
+
+    var modules = { strings : [], expressions : [] };
+    if (opts.nodes) modules.nodes = [];
+
+    if (src.indexOf(word) == -1) return modules;
+
+    walk(src, function (node) {
+        if (!isRequire(node)) return;
+        if (node.arguments.length
+        && node.arguments[0].type === 'Literal') {
+            modules.strings.push(node.arguments[0].value);
+        }
+        else {
+            modules.expressions.push(escodegen.generate(node.arguments[0]));
+        }
+        if (opts.nodes) modules.nodes.push(node);
+    });
+
+    return modules;
+};
 
 module.exports = function(mod, g) {
   if (g.opts.noParse && g.opts.noParse.indexOf(mod.filename) > -1) return
-  var deps = detective(mod.source, {word: 'require_async'})
+  var deps = extract(mod.source, {word: 'require.async'})
 
   return g.resolveDeps(deps, mod)
     .then(function(deps) { return {deps: deps, async_deps: deps} })
